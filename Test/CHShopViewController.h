4a92aa3b9cf12bf2eae1fd15dce36cd6bf25d2fe
jiangchenyi <jiangchenyi@jiangchenyideMacBook-Pro.local>
2015-07-16 17:13:19 +0800

    add shop view controller

---
+++ Test/CHShopViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHShopViewController.h
+//  Test
+//
+//  Created by jiangchenyi on 15/7/16.
+//  Copyright (c) 2015年 jiangchenyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHShopViewController : UIViewController
+
+@end
